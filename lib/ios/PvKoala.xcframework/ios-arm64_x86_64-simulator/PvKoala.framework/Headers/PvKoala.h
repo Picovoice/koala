ddfbb1d06e2814904cb4351d17d7af716269db50
Dominik Bunger <dominik.buenger@gmail.com>
2023-02-03 13:33:37 -0800

    Re-add iOS lib

---
+++ lib/ios/PvKoala.xcframework/ios-arm64_x86_64-simulator/PvKoala.framework/Headers/PvKoala.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for PvKoala.
+FOUNDATION_EXPORT double PvKoalaVersionNumber;
+
+//! Project version string for PvKoala.
+FOUNDATION_EXPORT const unsigned char PvKoalaVersionString[];
