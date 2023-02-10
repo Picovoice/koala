e6c930a809af397d6db63644b93cc8aa5f9c8fd3
Ian Lavery <ian@picovoice.ai>
2023-02-10 11:59:42 -0800

    rev

--- demo/ios/KoalaDemo/KoalaDemo/ViewModel.swift
+++ demo/ios/KoalaDemo/KoalaDemo/ViewModel.swift
@@ -21,7 +21,7 @@ enum UIState {
 
 class ViewModel: ObservableObject {
 
-    private let ACCESS_KEY = "Tw4jothrMMLyRYQ793yD/XF3DeithcbeNVsYlNN0Dc1vY26suWNOkg==" // Obtained from Picovoice Console (https://console.picovoice.ai)
+    private let ACCESS_KEY = "${YOUR_ACCESS_KEY_HERE}" // Obtained from Picovoice Console (https://console.picovoice.ai)
 
     private let AUDIO_FORMAT = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: Double(Koala.sampleRate), channels: 1, interleaved: true)!
 
