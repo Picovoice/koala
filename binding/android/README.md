8cae6fc84444da97cf487e22d2e243dea00d527f
Eric Mikulin <ErisMik@users.noreply.github.com>
2023-02-14 12:58:29 -0800

    v1.0 android (#8)

---
+++ binding/android/README.md
@@ -0,0 +1,80 @@
+# Koala Binding for Android
+
+## Koala Noise Suppression Engine
+
+Made in Vancouver, Canada by [Picovoice](https://picovoice.ai)
+
+Koala is an on-device noise suppression engine. Koala is:
+
+- Private; All voice processing runs locally.
+- Cross-Platform:
+  - Linux (x86_64), macOS (x86_64, arm64), Windows (x86_64)
+  - Android and iOS
+  - Chrome, Safari, Firefox, and Edge
+  - Raspberry Pi (4, 3) and NVIDIA Jetson Nano
+
+## Compatibility
+
+- Android 5.0 (SDK 21+)
+
+## Installation
+
+Koala is hosted on Maven Central. To include the package in your Android project, ensure you have
+included `mavenCentral()` in your top-level `build.gradle` file and then add the following to your
+app's `build.gradle`:
+
+```groovy
+dependencies {
+    // ...
+    implementation 'ai.picovoice:koala-android:${VERSION}'
+}
+```
+
+## AccessKey
+
+Koala requires a valid Picovoice `AccessKey` at initialization. `AccessKey` acts as your credentials when using Koala SDKs.
+You can get your `AccessKey` for free. Make sure to keep your `AccessKey` secret.
+Signup or Login to [Picovoice Console](https://console.picovoice.ai/) to get your `AccessKey`.
+
+## Usage
+
+Create an instance of the engine with the Koala Builder class by passing in the `accessKey` and Android app context:
+
+```java
+import ai.picovoice.koala.*;
+
+final String accessKey = "${ACCESS_KEY}"; // AccessKey provided by Picovoice Console (https://console.picovoice.ai/)
+try {
+    Koala koala = new Koala.Builder()
+        .setAccessKey(accessKey)
+        .build(appContext);
+} catch (KoalaException ex) { }
+```
+
+Enhance audio:
+
+```java
+short[] getNextAudioFrame() {
+    // .. get audioFrame
+    return audioFrame;
+}
+
+while true {
+    short[] enhancedFrame = koala.process(getNextAudioFrame());
+}
+```
+
+In case the next audio frame does not directly follow the previous one, call `reset()`:
+```java
+koala.reset();
+```
+
+When done, resources have to be released explicitly:
+
+```java
+koala.delete();
+```
+
+## Demo App
+
+For example usage refer to our [Android demo application](../../demo/android).
---
+++ demo/android/Activity/koala-activity-demo-app/src/androidTest/java/ai/picovoice/koalaactivitydemo/PerformanceTest.java
@@ -0,0 +1,147 @@
+/*
+    Copyright 2023 Picovoice Inc.
+    You may not use this file except in compliance with the license. A copy of the license is
+    located in the "LICENSE" file accompanying this source.
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+    express or implied. See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+package ai.picovoice.koalaactivitydemo;
+
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import ai.picovoice.koala.Koala;
+
+@RunWith(AndroidJUnit4.class)
+public class PerformanceTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+    Context testContext;
+    Context appContext;
+    AssetManager assetManager;
+    String testResourcesPath;
+    String accessKey;
+
+    @After
+    public void TearDown() {
+        reportHelper.label("Stopping App");
+    }
+
+    @Before
+    public void Setup() throws IOException {
+        testContext = InstrumentationRegistry.getInstrumentation().getContext();
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assetManager = testContext.getAssets();
+        extractAssetsRecursively("test_resources");
+        testResourcesPath = new File(appContext.getFilesDir(), "test_resources").getAbsolutePath();
+
+        accessKey = appContext.getString(R.string.pvTestingAccessKey);
+    }
+
+    @Test
+    public void testPerformance() throws Exception {
+        String iterationString = appContext.getString(R.string.numTestIterations);
+        String thresholdString = appContext.getString(R.string.performanceThresholdSec);
+        Assume.assumeNotNull(thresholdString);
+        Assume.assumeFalse(thresholdString.equals(""));
+
+        int numTestIterations = 100;
+        try {
+            numTestIterations = Integer.parseInt(iterationString);
+        } catch (NumberFormatException ignored) {}
+        double performanceThresholdSec = Double.parseDouble(thresholdString);
+
+        Koala koala = new Koala.Builder().setAccessKey(accessKey).build(getApplicationContext());
+        File testAudio = new File(testResourcesPath, "audio/test.wav");
+
+        long totalNSec = 0;
+        for (int i = 0; i < numTestIterations; i++) {
+            FileInputStream audioInputStream = new FileInputStream(testAudio);
+
+            byte[] rawData = new byte[koala.getFrameLength() * 2];
+            short[] pcm = new short[koala.getFrameLength()];
+            ByteBuffer pcmBuff = ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN);
+
+            audioInputStream.skip(44);
+
+            while (audioInputStream.available() > 0) {
+                int numRead = audioInputStream.read(pcmBuff.array());
+                if (numRead == koala.getFrameLength() * 2) {
+                    pcmBuff.asShortBuffer().get(pcm);
+                    long before = System.nanoTime();
+                    koala.process(pcm);
+                    long after = System.nanoTime();
+                    totalNSec += after - before;
+                }
+            }
+        }
+        koala.delete();
+
+        double avgNSec = totalNSec / (double) numTestIterations;
+        double avgSec = ((double) Math.round(avgNSec * 1e-6)) / 1000.0;
+        assertTrue(
+                String.format("Expected threshold (%.3fs), process took (%.3fs)", performanceThresholdSec, avgSec),
+                avgSec <= performanceThresholdSec
+        );
+    }
+
+    private void extractAssetsRecursively(String path) throws IOException {
+        String[] list = assetManager.list(path);
+        if (list.length > 0) {
+            File outputFile = new File(appContext.getFilesDir(), path);
+            if (!outputFile.exists()) {
+                outputFile.mkdirs();
+            }
+
+            for (String file : list) {
+                String filepath = path + "/" + file;
+                extractAssetsRecursively(filepath);
+            }
+        } else {
+            extractTestFile(path);
+        }
+    }
+
+    private void extractTestFile(String filepath) throws IOException {
+        InputStream is = new BufferedInputStream(assetManager.open(filepath), 256);
+        File absPath = new File(appContext.getFilesDir(), filepath);
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(absPath), 256);
+        int r;
+        while ((r = is.read()) != -1) {
+            os.write(r);
+        }
+        os.flush();
+
+        is.close();
+        os.close();
+    }
+}
