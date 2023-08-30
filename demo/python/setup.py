c98a090f10133edefb080436255db9e90a3a9405
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-08-30 15:10:55 -0700

    update pvrecorder

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -24,7 +24,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvkoalademo",
-    version="1.0.2",
+    version="1.0.3",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Koala Noise Suppression Engine demos",
@@ -32,7 +32,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/koala",
     packages=["pvkoalademo"],
-    install_requires=["pvkoala==1.0.1", "pvrecorder==1.1.1"],
+    install_requires=["pvkoala==1.0.1", "pvrecorder==1.2.1"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
