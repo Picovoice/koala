69a978d1c6f75dca93dbc1131d6b78f1d11ac1de
Zhuojin Liu <liu@picovoice.ai>
2025-02-05 14:47:00 -0800

    Update demo package setup to read dependency versions from requirement file

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -22,6 +22,9 @@ with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
     long_description = f.read()
 
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
+    dependencies = f.read().strip().splitlines()
+
 setuptools.setup(
     name="pvkoalademo",
     version="2.0.3",
@@ -32,7 +35,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/koala",
     packages=["pvkoalademo"],
-    install_requires=["pvkoala==2.0.3", "pvrecorder==1.2.4"],
+    install_requires=dependencies,
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
