a72b62dac9cac615531e679201f757dfdafedd6e
Zhuojin Liu <liu@picovoice.ai>
2025-02-13 17:15:58 -0800

    Add formatter

--- demo/web/scripts/koala.js
+++ demo/web/scripts/koala.js
@@ -5,9 +5,9 @@ let enhancedBuffer;
 let outputFrames;
 
 window.onload = function () {
-  const audioContext = new (window.AudioContext || window.webKitAudioContext)(
-          {sampleRate: 16000}
-  );
+  const audioContext = new (window.AudioContext || window.webKitAudioContext)({
+    sampleRate: 16000,
+  });
 
   const originalAudioGain = audioContext.createGain();
   originalAudioGain.gain.value = 0;
@@ -53,25 +53,35 @@ window.onload = function () {
           if (f > INT16_MAX) i = INT16_MAX;
           if (f < INT16_MIN) i = INT16_MIN;
           return i;
-        })
+        }),
       );
 
       writeMessage("Processing audio file...");
       const splitPcm = [];
       await koala.reset();
-      for (let i = 0; i < (i16PCM.length - koala.frameLength + 1); i += koala.frameLength) {
+      for (
+        let i = 0;
+        i < i16PCM.length - koala.frameLength + 1;
+        i += koala.frameLength
+      ) {
         const split = i16PCM.slice(i, i + koala.frameLength);
         splitPcm.push(split);
         await koala.process(split);
       }
 
-      writeMessage("Waiting for Koala engine to finish processing audio file...");
+      writeMessage(
+        "Waiting for Koala engine to finish processing audio file...",
+      );
       await waitForProcess(splitPcm, outputFrames);
 
       originalBuffer = createBuffer(i16PCM);
-      enhancedBuffer = createBuffer(mergeFrames(outputFrames, koala.delaySample));
+      enhancedBuffer = createBuffer(
+        mergeFrames(outputFrames, koala.delaySample),
+      );
 
-      writeMessage("Press 'Play' to listen to recording. Move the slider to play around with noise.");
+      writeMessage(
+        "Press 'Play' to listen to recording. Move the slider to play around with noise.",
+      );
       resultBox.style.display = "block";
     });
   });
@@ -93,8 +103,8 @@ window.onload = function () {
           audioData.push(event.data.inputFrame);
           break;
       }
-    }
-  }
+    },
+  };
 
   recordButton.addEventListener("click", async () => {
     displayTimer.style.display = "inline";
@@ -112,9 +122,12 @@ window.onload = function () {
     try {
       writeMessage("Recording audio...");
       window.WebVoiceProcessor.WebVoiceProcessor.setOptions({
-        frameLength: koala.frameLength
+        frameLength: koala.frameLength,
       });
-      await window.WebVoiceProcessor.WebVoiceProcessor.subscribe([recorderEngine, koala]);
+      await window.WebVoiceProcessor.WebVoiceProcessor.subscribe([
+        recorderEngine,
+        koala,
+      ]);
       timer = setInterval(() => {
         currentTimer += 0.1;
         displayTimer.innerText = `${currentTimer.toFixed(1)} / 120`;
@@ -132,16 +145,21 @@ window.onload = function () {
     stopRecord.style.display = "none";
     recordButton.style.display = "inline";
 
-    await window.WebVoiceProcessor.WebVoiceProcessor.unsubscribe([recorderEngine, koala]);
+    await window.WebVoiceProcessor.WebVoiceProcessor.unsubscribe([
+      recorderEngine,
+      koala,
+    ]);
     clearInterval(timer);
 
-    writeMessage("Waiting for Koala engine to finish processing...")
+    writeMessage("Waiting for Koala engine to finish processing...");
     await waitForProcess(audioData, outputFrames);
 
     originalBuffer = createBuffer(mergeFrames(audioData));
     enhancedBuffer = createBuffer(mergeFrames(outputFrames, koala.delaySample));
 
-    writeMessage("Press 'Play' to listen to recording. Move the slider to play around with noise.");
+    writeMessage(
+      "Press 'Play' to listen to recording. Move the slider to play around with noise.",
+    );
     resultBox.style.display = "block";
   });
 
@@ -168,14 +186,14 @@ window.onload = function () {
       enhancedAudioSource.connect(enhancedAudioGain);
       enhancedAudioSource.start(current_time + 0.2);
 
-      playAudio.innerHTML = "Stop"
+      playAudio.innerHTML = "Stop";
     } else {
       isPlaying = false;
 
       originalAudioSource.stop();
       enhancedAudioSource.stop();
 
-      playAudio.innerHTML = "Play"
+      playAudio.innerHTML = "Play";
     }
   });
 
@@ -196,7 +214,7 @@ window.onload = function () {
   function createBuffer(data) {
     const buffer = audioContext.createBuffer(1, data.length, koala.sampleRate);
     const source = new Float32Array(data.length);
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
       source[i] = data[i] < 0 ? data[i] / 32768 : data[i] / 32767;
     }
     buffer.copyToChannel(source, 0);
@@ -204,15 +222,15 @@ window.onload = function () {
   }
 
   async function waitForProcess(input, output) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setInterval(() => {
         if (input.length === output.length) {
           resolve();
         }
-      }, 100)
+      }, 100);
     });
   }
-}
+};
 
 function writeMessage(message) {
   console.log(message);
@@ -231,19 +249,19 @@ async function startKoala(accessKey) {
   writeMessage("Koala is loading. Please wait...");
   try {
     koala = await KoalaWeb.KoalaWorker.create(
-            accessKey,
-            processCallback,
-            { base64: modelParams },
-            { processErrorCallback: processErrorCallback }
+      accessKey,
+      processCallback,
+      { base64: modelParams },
+      { processErrorCallback: processErrorCallback },
     );
 
     writeMessage("Koala worker ready!");
 
     writeMessage(
-            "WebVoiceProcessor initializing. Microphone permissions requested ..."
+      "WebVoiceProcessor initializing. Microphone permissions requested ...",
     );
     window.WebVoiceProcessor.WebVoiceProcessor.setOptions({
-      frameLength: koala.frameLength
+      frameLength: koala.frameLength,
     });
     document.getElementById("control").style.display = "block";
     writeMessage("Koala worker is ready!");
